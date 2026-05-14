8a74c6ed10450e60d1617335f1cf157bc2bf2764
InakiMedina <inaki.medina@gmail.com>
2026-05-14 11:54:53 -0600

    fixed deploy so it dosent move on till its sure it works

--- src/main.py
+++ src/main.py
@@ -7,6 +7,11 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Catalog Service")
 
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
 # Include Routers
 app.include_router(clients.router)
 app.include_router(products.router)
