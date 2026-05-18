2c77e78747ca7292750243217ac6702b065be9e7
InakiMedina <inaki.medina@gmail.com>
2026-05-17 19:52:34 -0600

    sends eips to core

--- terraform/output.tf
+++ terraform/output.tf
@@ -7,3 +7,8 @@ output "catalog_public_ip" {
   description = "Stable public IPv4 (Elastic IP) for Catalog"
   value       = aws_eip.catalog.public_ip
 }
+
+output "catalog_backend_url" {
+  description = "Base URL written to Core CATALOG_BACKEND_URL"
+  value       = "http://${aws_eip.catalog.public_ip}:80"
+}
